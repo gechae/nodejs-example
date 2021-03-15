2a8bbe2332e758805d4d7038a751e91e557ec189
gechae <light03@naver.com>
2021-03-15 17:56:48 +0900

    사용자 페이지 만들어보기

--- nodejs-example-02/app.js
+++ nodejs-example-02/app.js
@@ -4,15 +4,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+//DB 연결정보 확인 하기위해 require
+const models = require("./models/v1/index");
 
-const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
 
 const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
-app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,6 +45,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// database 연결 정보 확인하는 함수
+models.sequelize.sync().then( () => {
+  console.log(" DB 연결 성공");
+}).catch(err => {
+  console.log("연결 실패");
+  console.log(err);
 });
 
 module.exports = app;
--- nodejs-example-02/routes/index.js
+++ nodejs-example-02/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({'title': "https://dundun-dev.tistory.com/64?category=725300"});
+  res.render("main.ejs")
+  //res.json({'title': "https://dundun-dev.tistory.com/64?category=725300"});
 });
 
 router.use('/v1', require('./v1'));
